88f5c73fb140dfd2cb6cf098b26210dbca8ab2be
한은이 <haneuneee@gmail.com>
2024-08-04 23:47:34 +0900

    Quartz sync: Aug 4, 2024, 11:47 PM

--- quartz.config.ts
+++ quartz.config.ts
@@ -17,7 +17,7 @@ const config: QuartzConfig = {
     },
     locale: "ko-KR",
     baseUrl: "tunapanini.xyz",
-    ignorePatterns: ["private", "templates", ".obsidian", "_*", "robot.txt"],
+    ignorePatterns: ["private", "templates", ".obsidian", "_*"],
     defaultDateType: "created",
     theme: {
       fontOrigin: "googleFonts",
