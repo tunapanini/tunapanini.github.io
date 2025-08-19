08f657318dfde5a14ee03bd5619acf598d9349aa
tunapanini <tunapanini@kakao.com>
2025-08-19 19:23:39 +0900

    Update defaultDateType to "modified" and adjust priority in transformers

--- quartz.config.ts
+++ quartz.config.ts
@@ -18,7 +18,7 @@ const config: QuartzConfig = {
     locale: "ko-KR",
     baseUrl: "tunapanini.xyz",
     ignorePatterns: ["private", "templates", ".obsidian", "_*"],
-    defaultDateType: "git",
+    defaultDateType: "modified",
     theme: {
       fontOrigin: "googleFonts",
       cdnCaching: true,
@@ -57,7 +57,7 @@ const config: QuartzConfig = {
     transformers: [
       Plugin.FrontMatter(),
       Plugin.CreatedModifiedDate({
-        priority: ["frontmatter", "filesystem"],
+        priority: ["frontmatter", "git", "filesystem"],
       }),
       Plugin.SyntaxHighlighting({
         theme: {
