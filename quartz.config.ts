8f48770b2afd1c85c58852694c6e43b0ebe08e99
tunapanini <tunapanini@kakao.com>
2025-08-19 19:15:53 +0900

    Update defaultDateType to "git" in quartz.config.ts

--- quartz.config.ts
+++ quartz.config.ts
@@ -18,7 +18,7 @@ const config: QuartzConfig = {
     locale: "ko-KR",
     baseUrl: "tunapanini.xyz",
     ignorePatterns: ["private", "templates", ".obsidian", "_*"],
-    defaultDateType: "modified",
+    defaultDateType: "git",
     theme: {
       fontOrigin: "googleFonts",
       cdnCaching: true,
